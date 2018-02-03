2cd0c62d5d894039a6c85610804e8de39e5e9e96
saewashi <35227909+saewashi@users.noreply.github.com>
2018-02-03 12:39:26 -0500

    Exercises 02a
    Second exercises to help prepare for the second assignment for next week.

--- README.md
+++ README.md
@@ -1,23 +1,7 @@
 # 02a-Exercises-Functions-and-Dictionaries
 
-Here are some exercises to help you learn about Python functions, sets, and dictionaries. This is the second set of exercises for ILS-Z399. To complete these exercises:
-
-Open the IDLE program that came with your Python installation. Use IDLE to interpret the Python code and answer the questions in the file.
-
-Comments in Python are marked by a # sign (for single-line comments) or three matching quotation marks (''' or """) if a comment requires more than one line.
-
-In the exercise file, you will see a series of Python statements followed by comment lines. Run the Python statements in IDLE and then add the answer to the comment on the following line.
-
-For example, if the exercise contained these statements:
-~~~ 
-10 + 15
-#
-~~~
-You would enter the statement 10 + 15 into IDLE. After hitting enter, IDLE returns 25, and so you would copy that response after the # on the following line.
-
-Some of the exercises require you to interpret what is going on. Feel free to explore other variations in IDLE until you can come up with a reasonable explanation. 
-
-As with your assignments, please edit the LICENSE file (replace the [year] [fullname] with the current year and your name), and edit this file to describe what your code does.
+ This program is preparation for the 3rd assignment, dealing with functions and dictionaries.
+ This is a program with exercises that we must copy into IDLE, and describe what is happening.
 
 ---
 
--- exercises.py
+++ exercises.py
@@ -6,7 +6,7 @@ These skills can be used in assignment 2. These exercises are designed to teach 
 
 To answer these exercises, open the IDLE program that came with your Python installation. IDLE is a line-by-line Python interpreter.
 You can copy lines from this file into IDLE to interpret them and produce a result. Then copy the result back to the following line in this file (after the #).
-You will also need to answer several questions to show you understand what is happening. 
+You will also need to answer several questions to show you understand what is happening.
 
 
 '''
@@ -18,7 +18,9 @@ for e in example_list:
 print(s)
 #
 # What do these five lines of code do?
-#
+# I got this: 38
+# supposed to do, I would say that it is supposed to take  the numbers in the list that is assigned to "s"
+# and "e" to produce the addition outcome.
 
 # --------------------------------------------------
 
@@ -33,15 +35,26 @@ example_set = example_set - example_set_2
 print(example_set)
 set_to_list = list(example_set)
 print(set_to_list)
-#
+#{1, 2, 3, 4}
+# 3 is in the set
+# 4 is in the set
+# {1, 2, 3}
+# [1, 2, 3]
 #
 #
 #
 #
 # What qualities of a python set do you see in the (above) example?
-#
+
+# The qualities that I see of a python set, is that it finds the nummbers in a specfic set, and
+# prints the set. It also finds the same repeating numbers in two sets.
+
 # In what situations might a python set be a useful data structure?
-#
+#  A python set may be useful data structure when there are items within the set, that the user-
+# may want to find without having to search through all the sets themselves to find data.
+# For example, if we wanted to find someone within a public file but didn't want to search through all-
+# the people within that public file, they could create a data structure that finds specfic people
+# with a specific name that seperates them from all the other names on file.
 
 # --------------------------------------------------
 
@@ -55,15 +68,28 @@ students = ['James','Paige','George','Ruth','Gwen','Claire']
 example_dictionary['students'] = students
 print(example_dictionary)
 print(example_dictionary['students'][2])
-#
-#
+
+#{'class': 'Z-399', 'teacher': 'Jason Francis', 'time': 9.0, 5: 12345}
+# 12345
+# {'class': 'Z-399', 'teacher': 'Jason Francis', 'time': '9:00', 5: 12345}
+# {'class': 'Z-399', 'teacher': 'Jason Francis', 'time': '9:00', 5: 12345, 'students': ['James', 'Paige', 'George', 'Ruth', 'Gwen', 'Claire']}
+# George
 #
 #
 #
 # What qualities of a python dictionary do you see in the (above) example?
-#
+# The qualities that I see in this python dictionary, is that it finds the name of the person, the class, the teacher, the time,
+# and the class section number and pulls all of the data from the dictionary about that class.
+
 # Write a python dictionary that might describe a bicycle. I'll get you started. Think about what qualities a bicycle could have (top speed, mileage, pedals, seat height, etc)
-bicycle = { 'color':'', 'wheels':[] }
+bicycle = { 'color':'Blue', 'wheels': 'Speedy Wheels', 'model:''Speed bikes':1966 }
+print(bicycle)
+bicycle['model'] = 'Speedy Bikes'
+print(bicyle)
+bicycle['color'] = 'Blue'
+print(bicycle)
+bicycle['wheels'] = 'Speedy Wheels,' '1966'
+print(bicyle)
 
 # --------------------------------------------------
 
@@ -77,11 +103,12 @@ r = product_list([1,2,3])
 print(r)
 print(product_list([2,5,6]))
 print(product_list([-2,3,-4,5,-6]))
-#
-#
+# 6
+# 60
+# -720
 #
 # What is happening in the (above) example?
-#
+# What is happening i the above example is that the list is multplying all the numbers together in each list to get the ending multiplication result.
 
 # --------------------------------------------------
 
@@ -109,17 +136,31 @@ for y in years:
 	else:
 		print(str(y) + ': Just 365 for me')
 #
-#
+#2017: Just 365 for me
+#2018: Just 365 for me
+#2000: I get an extra day this year!
+#2100: Just 365 for me
+# 2400: I get an extra day this year!
+#This isn't even a year: Just 365 for me
 #
 #
 #
 #
 # What is happening in the (above) example?
-#
+
+# What is happening in the example above, is that we are defining a function that is checking to see if-
+# each year has a leap year for the following years. Ex: if 2017 doesn't have a leap year, then the programs runs-
+# it has false, and the same goes for True.
+
 # What is the purpose for the multi-line comment at the beginning of the is_leap_year function?
-#
+
+# The purpose of the multi-line comment is that it describes what the function is supposed to do, -
+# and it's helpful if someone has a long comment to input.
+
 # What happens if you pass a value to the function that isn't a year? Why?
-#
+#  If you pass a value to the function that isn't a year, then it would produce an error.
+# The reason being, is that the function is designed to check and see if each year is a regular year, -
+# or a leap year. If a value isn't a year, then there's no way for the function to perform the Boolean.
 
 # --------------------------------------------------
 
@@ -145,7 +186,16 @@ for i in range(0,10):
 		print('%d: buzz'%i)
 	else:
 		print(i)
-#
+#0: fizzbuzz
+# 1
+# 2: buzz
+# 3: fizz
+# 4: buzz
+# 5
+# 6: fizzbuzz
+# 7
+# 8: buzz
+# 9: fizz
 #
 #
 #
@@ -156,13 +206,24 @@ for i in range(0,10):
 #
 #
 # What is happening in this example?
-#
+
+# What is happening in this example is that the function taking a range of numbers, and testing if each number -
+# will produce a fizz, buzz, or fizzbuzz, in a Boolean statement.
+
 # How would you alter the program so that it prints fizz on multiples of 5 and buzz on multiples of 4?
-#
+
+# How I would alter the program so that it prints fizz on multiples of 5 and buzz on 4, is change the two -
+# numbers in the if statements.
+
 # We actually don't want the program to print fizzbuzz when i <= 0. How would you fix this problem?
-#
+
+#  How I would fix that is by changing the values in the if statements so that each time it runs -
+# past a certain nummber in the range, it won't print fizzbuzz.
+
 # How would you alter the program so that it prints your name whenever both conditions are met?
-#
+
+# How I would alter the prgram so that it prints my name whenever both conditions are met, -
+# is to add another if statement that contains my name, and prints after the conditions are met.
 
 # --------------------------------------------------
 
@@ -194,8 +255,8 @@ def get_option(options):
 			print('Please enter one of the options')
 			to_return = 0
 	return to_return
-			
-	
+
+
 script = [
 	{
 		'location':1
@@ -219,10 +280,19 @@ script = [
 starting = 0
 get_option(script[starting]['options'])
 # What is happening in this block of code?
-#
+# What is happeninh in this block of code, is that it is introducting the user to a storyline game.
+# It is specifically giving the user options to choose from, and whichever the user inputs,-
+# it leads to another storyline by the choices they make.
+
 # What does the get_option function do?
-#
+
+# get_option stores the list of options that the user has to choose from, instead of each option -
+# being written individually.
+
 # How would you display the description of the current location?
-#
+# How I would display the description of the current location, is being in a haunted house, and it's storming,
+# the user can either run away, or they can try and open the door which is locked.
+
 # How would you display the description of the next location (after selecting an option)?
-#
+# I would display the description as a narrator telling the person that the door was locked,
+# but that they decided to do it anyway.
